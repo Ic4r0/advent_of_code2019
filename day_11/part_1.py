39b49bdf82e0f77cfdd8fb10f507df0743b8d25d
Ic4r0 <nicola@klee.eu>
2020-04-13 22:22:17 +0200

    Added (incomplete) code for day 11 part 1

--- day_11/part_1.py
+++ day_11/part_1.py
@@ -53,8 +53,77 @@ def parameter_mode(int_stream: list,
 
     return output_first_par, output_second_par, output_third_par
 
-def intcode_computer(int_stream: list,
-                     intcode_input: int):
+def movement(current_direction: str,
+             previous_positions: list,
+             output_param: int):
+    curr_x, curr_y = previous_positions[-1][1]
+    if current_direction == 'up':
+        if output_param == 0:
+            new_pos = (curr_x-1, curr_y)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'left'
+        else:
+            new_pos = (curr_x+1, curr_y)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'right'
+    elif current_direction == 'down':
+        if output_param == 0:
+            new_pos = (curr_x+1, curr_y)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'right'
+        else:
+            new_pos = (curr_x-1, curr_y)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'left'
+    elif current_direction == 'left':
+        if output_param == 0:
+            new_pos = (curr_x, curr_y-1)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'down'
+        else:
+            new_pos = (curr_x, curr_y+1)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'up'
+    elif current_direction == 'right':
+        if output_param == 0:
+            new_pos = (curr_x, curr_y+1)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'up'
+        else:
+            new_pos = (curr_x, curr_y-1)
+            if ['w', new_pos] in previous_positions:
+                previous_positions.append(['w', new_pos])
+            else:
+                previous_positions.append(['b', new_pos])
+            new_direction = 'down'
+    return new_direction
+
+def intcode_computer(int_stream: list):
+    panels = [['b', (0, 0)]]
+    direction = 'up'
+    color_set = False
+
     # Start program
     index = 0
     relative_base = 0
@@ -77,22 +146,35 @@ def intcode_computer(int_stream: list,
                 int_stream[third_par] = first_par * second_par
             index += 4
         elif int(opcode[-1]) == 3:  # input opcode
+            if panels[-1][0] == 'b':
+                input_value = 0
+            else:
+                input_value = 1
             if int(opcode[2]) == 0:
                 try:
-                    int_stream[int_stream[index + 1]] = intcode_input
+                    int_stream[int_stream[index + 1]] = input_value
                 except:
                     int_stream.extend([0] * (int_stream[index + 1] - len(int_stream) + 1))
-                    int_stream[int_stream[index + 1]] = intcode_input
+                    int_stream[int_stream[index + 1]] = input_value
             elif int(opcode[2]) == 2:
                 try:
-                    int_stream[int_stream[index + 1] + relative_base] = intcode_input
+                    int_stream[int_stream[index + 1] + relative_base] = input_value
                 except:
                     int_stream.extend([0] * (int_stream[index + 1] + relative_base - len(int_stream) + 1))
-                    int_stream[int_stream[index + 1] + relative_base] = intcode_input
+                    int_stream[int_stream[index + 1] + relative_base] = input_value
             index += 2
         elif int(opcode[-1]) == 4:  # output opcode
             first_par, _, _ = parameter_mode(int_stream, index, opcode, relative_base, second_par=False, third_par=False)
-            print(first_par)
+            if not color_set:
+                if first_par == 0:
+                    panels[-1][0] = 'b'
+                else:
+                    panels[-1][0] = 'w'
+                color_set = True
+            else:
+                color_set = False
+                direction = movement(direction, panels, first_par)
+
             index += 2
         elif int(opcode[-1]) == 5:  # jump-if-true opcode
             first_par, second_par, _ = parameter_mode(int_stream, index, opcode, relative_base, third_par=False)
@@ -138,6 +220,7 @@ def intcode_computer(int_stream: list,
             first_par, _, _ = parameter_mode(int_stream, index, opcode, relative_base, second_par=False, third_par=False)
             relative_base += first_par
             index += 2
+    return panels
 
 dirname, _ = os.path.split(os.path.abspath(__file__))
 file_path = dirname + "\\input"
@@ -149,6 +232,7 @@ with open(file_path) as file:
     for line in file:
         int_code.extend([int(elem) for elem in line.split(',')])
 
-intcode_computer(int_stream=int_code, intcode_input=INPUT)
-
-#git merge origin/master --no-commit --no-ff
+steps_made = intcode_computer(int_stream=int_code)
+# visited_panels = list(dict.fromkeys([elem for _, elem in steps_made]))
+# print(len(visited_panels)-1)
+print(steps_made)
