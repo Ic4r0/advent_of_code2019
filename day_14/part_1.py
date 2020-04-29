016e7bb2908c7b81e870b72a71d1799c4e2ce5c8
Ic4r0 <nicola@klee.eu>
2020-04-30 00:09:22 +0200

    Added day 14 starting files

---
+++ day_14/part_1.py
@@ -0,0 +1,53 @@
+"""--- Day 14: Space Stoichiometry - Part One ---"""
+
+import os
+import re
+from copy import deepcopy
+
+class Reaction:
+
+    def __init__(self, reactions: dict):
+        self.reactions = deepcopy(reactions)
+        # self.output_elements = self.list_of_op_elements()
+        # print(self.output_elements)
+
+    # def list_of_op_elements(self):
+    #     output_list = []
+    #     for key, _ in self.reactions.items():
+    #         if key[1] not in output_list:
+    #             output_list.append(key[1])
+    #     return output_list
+    
+    def ore_cost_for_elem(self,
+                          qnt_elem: int,
+                          elem: str,
+                          start_remains: dict = {}):
+        op_chem, in_chem = [(k, v) for k, v in self.reactions.items()
+                                                        if k[1] == elem].pop()
+        if op_chem[0] < qnt_elem:
+            multiplier = qnt_elem // op_chem[0] + (qnt_elem % op_chem[0] > 0)
+            in_chem = [(multiplier*k, v) for k, v in in_chem]
+            if op_chem[0] * multiplier > qnt_elem:
+                start_remains[op_chem[1]] = op_chem[0] * multiplier - qnt_elem
+        elif op_chem[0] > qnt_elem:
+            start_remains[op_chem[1]] = op_chem[0] - qnt_elem
+
+dirname, _ = os.path.split(os.path.abspath(__file__))
+file_path = dirname + "\\input"
+
+# Open the input file
+with open(file_path) as file:
+    # Create a list where we store each element of the input
+    reaction_dict = {}
+    for line in file:
+        match = re.findall(r"(\d+ \w+)", line)
+        output_chemical = tuple(match.pop().split())
+        output_chemical = (int(output_chemical[0]), output_chemical[1])
+        input_chemical = [tuple(elem.split()) for elem in match]
+        for idx in range(len(input_chemical)):
+            qnt, element = input_chemical[idx]
+            input_chemical[idx] = (int(qnt), element)
+        reaction_dict[output_chemical] = input_chemical
+
+reaction = Reaction(reaction_dict)
+# reaction.ore_cost_for_elem(1, 'FUEL')
